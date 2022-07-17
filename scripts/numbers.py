2025a13c71a26631898724aa5eba127ff5fd493e
ariasAleia <ariasfaleja@gmail.com>
2022-07-17 17:06:23 -0500

    Test some functions with numbers

---
+++ scripts/numbers.py
@@ -0,0 +1,16 @@
+print(3 + 7.8)
+print(10 % 3)
+
+my_num = -8
+print(my_num)
+print(str(my_num))
+
+print(abs(my_num))
+
+print(pow(3, 4))
+
+print(max(45, 34))
+
+print(min(34, 2))
+
+print(round(3.2676877, 3))
