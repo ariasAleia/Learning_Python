e2ce578bb684c655bc80f493fb3d4a371c1cc2ac
ariasAleia <ariasfaleja@gmail.com>
2022-07-17 18:30:30 -0500

    Create a calculator
    Adds two integer numbers

---
+++ scripts/calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the super calculator yeahhh!\n")
+a = int(input("Enter the first integer: "))
+b = int(input("Enter the second integer: "))
+
+print("The sum of both numbers is " + str(a+b))
+
+print("Yeah, I know. This calculator is mega wow :D")
