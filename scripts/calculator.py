c4fcf274fc9fe512249facee62940923b4f8f645
ariasAleia <ariasfaleja@gmail.com>
2022-07-17 19:58:58 -0500

    Add new word to answer string in calculator

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -2,6 +2,6 @@ print("Welcome to the super calculator yeahhh!\n")
 a = float(input("Enter the first integer: "))
 b = float(input("Enter the second integer: "))
 
-print("The sum of both numbers is " + str(a+b))
+print("The sum of both numbers is chan chaaaan: " + str(a+b))
 
 print("Yeah, I know. This calculator is mega wow :D")
